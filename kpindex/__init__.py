aa5a063301cf55a6648387ff3e615e316d2c8d43
mattkjames7 <mattkjames7@gmail.com>
2021-12-13 15:55:10 +0000

    updated version

--- kpindex/__init__.py
+++ kpindex/__init__.py
@@ -1,3 +1,6 @@
+__version__ = '1.0.0'
+
+
 from . import Globals
 from .UpdateLocalData import UpdateLocalData
 from .ReadKp import ReadKp
--- setup.py
+++ setup.py
@@ -5,9 +5,36 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def getversion():
+	'''
+	read the version string from __init__
+	
+	'''
+	#get the init file path
+	thispath = os.path.abspath(os.path.dirname(__file__))+'/'
+	initfile = thispath + 'kpindex/__init__.py'
+	
+	#read the file in
+	f = open(initfile,'r')
+	lines = f.readlines()
+	f.close()
+	
+	#search for the version
+	version = 'unknown'
+	for l in lines:
+		if '__version__' in l:
+			s = l.split('=')
+			version = s[-1].strip().strip('"').strip("'")
+			break
+	return version
+	
+version = getversion()
+
+
+
 setuptools.setup(
     name="kpindex",
-    version="0.0.1",
+    version=version,
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="Very simple package for obtaining the kp index data",
@@ -19,6 +46,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX",
+		"Operating System :: MacOS :: MacOS X",
+		"Operating System :: Microsoft :: Windows",
     ],
     install_requires=[
 		'numpy',
