7b0aade3d2da65f1988fee6863c407326494f32a
matt <mattkjames7@gmail.com>
2025-03-20 23:53:43 +0000

    fixed version

---
+++ getversion.py
@@ -0,0 +1,24 @@
+import os
+
+def getversion():
+	'''
+	read the version string from __init__
+	
+	'''
+	#get the init file path
+	thispath = os.path.abspath(os.path.dirname(__file__))+'/'
+	initfile = thispath + 'kpindex/__init__.py'
+	
+	#read the file in
+	f = open(initfile,'r')
+	lines = f.readlines()
+	f.close()
+	
+	#search for the version
+	version = 'unknown'
+	for l in lines:
+		if '__version__' in l:
+			s = l.split('=')
+			version = s[-1].strip().strip('"').strip("'")
+			break
+	return version
--- setup.py
+++ setup.py
@@ -1,32 +1,11 @@
 import setuptools
 from setuptools.command.install import install
 import os
+from getversion import getversion
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-def getversion():
-	'''
-	read the version string from __init__
-	
-	'''
-	#get the init file path
-	thispath = os.path.abspath(os.path.dirname(__file__))+'/'
-	initfile = thispath + 'kpindex/__init__.py'
-	
-	#read the file in
-	f = open(initfile,'r')
-	lines = f.readlines()
-	f.close()
-	
-	#search for the version
-	version = 'unknown'
-	for l in lines:
-		if '__version__' in l:
-			s = l.split('=')
-			version = s[-1].strip().strip('"').strip("'")
-			break
-	return version
 	
 version = getversion()
 
